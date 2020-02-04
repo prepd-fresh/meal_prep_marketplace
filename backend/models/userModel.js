d2bb110aa0e3fe82a35e83b9da6dbb9ae89874b2
Adam <Adam.kita97@gmail.com>
2020-02-04 12:24:30 -0500

    Created model for user
    Created a model for the user with password incryption

---
+++ backend/models/userModel.js
@@ -0,0 +1,48 @@
+import { Schema, Mongoose } from 'mongoose';
+import { String } from 'prop-types';
+import bcrypt from 'bcrypt';
+import { runInNewContext } from 'vm';
+
+const Document, Shema, Model = require("mongoose")
+
+/*
+    Creates a Schema for the User object and what
+    values the user object will store
+*/
+export const userSchema = new Schema({
+    email: String,
+    name:String,
+    password:String,
+    name: String,
+    date: {type: Date, default: Date.now}
+
+})
+
+userSchema.pre("save",()=>{
+
+    //creates instances of the user
+    const user = this
+
+    // only hash the password if it has been modified (or is new)
+    if(!user.isModified('password')) return next();
+
+        // generate a salt
+        bcrypt.genSalt(10, function(err, salt) {
+            if (err) return next(err);
+    
+            // hash the password using our new salt
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) return next(err);
+    
+                // override the cleartext password with the hashed one
+                user.password = hash;
+                console.log(user.password)
+                next();
+    
+            });
+        });
+});
+
+const User = Mongoose.model('User', userSchema);
+
+export  {User}
